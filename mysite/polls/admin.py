6b51f60642bb97a19e9bfff1001b28a539a32510
Thiago Machado Lima <thiagol@dbserver.com.br>
2016-03-15 14:15:35 -0300

    Initial commit

---
+++ mysite/polls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
